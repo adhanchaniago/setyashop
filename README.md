bdab03502375c5be8f34c820beffa09b23f43398
Genta InformatikaG <69096671+hesendwi1411@users.noreply.github.com>
2020-08-02 13:45:08 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# setyashop
+ Company Profile
---
+++ application/views/front/v_tentang.php
@@ -0,0 +1,213 @@
+<html lang="id">
+    <!--<![endif]-->
+    <head>
+        <title>Lokasi Kantor</title>
+
+<!-- Meta tags -->
+       <meta charset="utf-8">        <meta name="description" content="Setya Shop Bantul Yogyakarta" />        <meta name="author" content="Genta Informatika" />        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="<?php echo base_url().'theme/css/base.css'?>" />
+        <link rel="stylesheet" href="<?php echo base_url().'theme/css/skeleton.css'?>" />
+        <link rel="stylesheet" href="<?php echo base_url().'theme/css/flexslider.css'?>" />
+        <link rel="stylesheet" href="<?php echo base_url().'theme/css/jquery.fancybox-1.3.4.css'?>" />
+        <link rel="stylesheet" href="<?php echo base_url().'theme/css/validationEngine.jquery.css'?>" />
+        <link rel="stylesheet" href="<?php echo base_url().'theme/css/smoothness/jquery-ui-1.8.22.custom.css'?>" />
+        <link rel="stylesheet" href="<?php echo base_url().'theme/css/ui.spinner.css'?>" />
+        <link rel="stylesheet" href="<?php echo base_url().'theme/css/lamoon.css'?>" />
+        <link href='http://fonts.googleapis.com/css?family=Lato|Lato:300|Vollkorn:400italic' rel='stylesheet' type='text/css'>
+
+        <!-- Favicons -->
+        <link rel="shortcut icon" href="<?php echo base_url().'theme/images/favicon.png'?>" />
+     <?php 
+            function limit_words($string, $word_limit){
+                $words = explode(" ",$string);
+                return implode(" ",array_splice($words,0,$word_limit));
+            }
+                
+        ?>
+    </head>
+    <body>
+        
+        <!-- Background Image -->
+        <img src="<?php echo base_url().'theme/images/bg1.jpg'?>" class="bg" alt="" />
+        
+        <!-- Root Container -->
+        <div id="root-container" class="container">
+            <div id="wrapper" class="sixteen columns">
+                
+                <!-- Banner -->
+                <div id="sub-banner">
+                    <div id="logo">
+                    </div>
+                    <img src="<?php echo base_url().'theme/images/placeholders/940x220.png'?>" alt="" />
+                </div>
+                
+               <!-- Main Menu -->
+                <div id="menu" class="home">
+                    <ul id="root-menu" class="sf-menu">
+                        <?php
+                        $this->load->view('front/menu');
+                        ?>
+                    </ul>
+                </div>
+               
+                <!-- Content -->
+                <div id="content" class="reservation">
+                    <div class="container section">
+                        <center><h3><span>About</span></h3></center>
+                        <div class="container section" style="margin-bottom: 400px;">
+                            Dari alam untuk alam. Istilah ini cocok digunakan untuk kemasan tradisonal. Kemasan sudah ada sejak jaman dahulu kala. Nenek moyang kita membungkus makanan menggunakan bahan yang berasal dari alam. Misalkan mengemas pisang dengan anyaman bambu, membungkus nasi dengan daun pisang disebut lontong, daging sapi dibungkus dengan daun jati, dan masih banyak lagi. </br></br>
+							Dengan perkembangan pola hidup masyarakat sekarang berubah, maka kemasan berubah lebih modern. Peningkatan industri dan kemajuan ilmu pengetahuan dan teknologi (IPTEK), juga menjadikan faktor perkembangan kemasan makanan. Fungsinya pun sekarang lebih beragam. </br></br>
+								Kemasan adalah wadah atau tempat yang digunakan untuk mengemas suatu produk yang dilengkapi dengan tulisan, label, keterangan lain yang menjelaskan isi, kegunaan dan informasi lain yang perlu disampiakn kepada konsumen. </br></br>
+							Fungsi kemasan: melindungi produk dari benturan ketika pengiriman; melindungi produk dari kerusakan fisik, kimia, dan biologis; Mencegah terjadinya kontaminasi bahan dari luar; Menjaga mutu selama penyimpanan; Sebagai pengawet makanan; Memenuhi standart mutu produk; Mempermudah pemakaian, penyimpanan, pendistribusian; Menampilkan identitas dan informasi produk, sehingga dapat meningkatkan daya tarik konsumen, peningkatan keuntungan, dan peningkatan daya saing. </br></br>
+							Dari segala kelebihan yang dimiliki kemasan, ada juga kelemahan dan kekurangannya. Yaitu pencemaran lingkungan yang disebabkan penggunaan nahan kemasan sintetik yang tidak ramah lingkungan; Harga kemasan yang lebih mahal dibanding dengan produk yang dikemas, sehingga harga jual produk tinggi; Kurang ekonomis dikarenakan sekali pakai kemudian dibuang; Tidak semua kemasan dilengkapi dengan informasi label yang jelas. Label yang baik harus memuat unsur-unsur : Nama bahan, nomor pendaftaran produk, komposisi bahan, kode produksi, berat/volume produk, aturan pemakaian, tanggal kadaluarsa, peringatan bahaya sampingan/ kontra indikasi, saran penyajian. </br></br>
+                        Bahan dan Bentuk Kemasan </br>
+•	Bentuk Kemasan</br>
+&emsp; Kemasan kaku (rigid), misal : logam, kayu, karton, gelas
+Kemasan semi kaku, misal: alumunium, folding karton, set up box
+Kemasan fleksibel, misal: kertas, plastik, alumunium foil </br>
+
+•	Bahan Kemasan</br>
+&emsp;1.	Kayu</br>
+&emsp;Pengemasan untuk produk yang memiliki bobot besar dan beresiko tinggi untuk rusak. Untuk pengemasan produk kecil tidak efektif. Apabila ditumpuk sangat kuat. Untuk distribusi juga baik. Namun kurang kedap air. Kemasan kayu juga membutuhkan ruang penyimpanan besar. Contoh: kayu utuk krat, kayu untuk pallet. </br>
+
+&emsp;2.	Logam</br>
+&emsp;Kemasan logam memiliki kekuatan mekanis yang baik. Jenis logam yang biasa digunakan untuk kemasan adalah baja dan kaleng logam. Baja digunakan untuk pengemasan produk cair, semi padat, dan tepung. Contoh minyak saus, manisan buah yang dikalengkan dan pelarut organik. Kaleng logam terbuat dari plat baja yang dilapisi timah dikedua sisinya. Contoh kaleng minuman soda. </br>
+
+&emsp;3.	Gelas</br>
+&emsp;Gelas telah sangat lama digunakan untuk kemasan produk. Gelas terbuat dari pasir, soda abu, kapur/campuran alkali. 
+Kelebihan gelas adalah bersifat transparan, tidak beraksi dengan produk, kedap terhadap gas air dan bau, keawetan aroma, rasa dan warna produk, dapat dibentuk dan didesain, mudah diwarnai, tahan sugu rendah dan sterilisasi, dapat divakum.
+Kelemahannya yaitu bersifat rapuh dan mudah pecah.
+Contoh : Produk minuman, saos, obat, bahan kimia, dan kosmetik </br>
+
+&emsp;4.	Kertas</br>
+&emsp;Kertas adalah bahan tipis yang dihasilkan dari proses pengepresan serat kayu yang berasal dari pulp. Kelebihan kemasan kertas adalah mudah dilipat ketika disimpan, mudah diwarnai (dicetak), harga relatif murah. Kekurangannya yaitu sekali pakai langsung dibuang, mudah rusak terkena air.
+
+						</div>
+                    </div>
+                </div>
+                
+                
+                <!-- Footer -->
+                <div id="footer">
+                    <div class="container section end">
+                        <div id="footer-about" class="one-fourth column">
+                            <p><img src="<?php echo base_url().'theme/images/footer-logo.png'?>" alt="" />
+                            </p>
+                            <p>
+								<br><a href="#">Alamat Kantor:</a></br>
+                                <span>Dsn. Iroyudan, RT.02,Guwosari, Pajangan, Bantul</span>
+								<span>Daerah Istimewa Yogyakarta 55712</span>
+                                
+                            </p>
+                            <p>
+                                <span>Telp: +62 812-1655-5657</span>
+                                <span>Email: heri@setyashop.com</span>
+                            </p>
+                        </div>
+                        <div id="footer-offers" class="one-fourth column">
+                            <h4><span class="footer left">News &amp; Events</span></h4>
+                            <ul>
+                             <?php
+                            foreach ($berita->result_array() as $j) {
+                                $idberitaf=$j['idberita'];
+                                $judulf=$j['judul'];
+                                $isif=limit_words($j['isi'],10);
+                                $tglpostf=$j['tglpost'];
+                                $gbrf=$j['gambar'];
+                                $userf=$j['user'];
+                            ?>
+                                <li>
+                                    <a href="<?php echo base_url().'berita_post/detail_berita/'.$idberitaf;?>"><img width="50" height="50" src="<?php echo base_url().'assets/gambars/blog/'.$gbrf;?>" alt="" /><?php echo $isif;?></a>
+                                </li>
+
+                                <?php } ?>
+                            </ul>
+                        </div>
+                        <div id="footer-offers" class="one-fourth column">
+                            <h4><span class="footer left">Produk</span></h4>
+                            <ul>
+                            <?php
+                            foreach ($paket->result_array() as $h) {
+                                $idpaketf=$h['idpaket'];
+                                $namapaketf=$h['nama_paket'];
+                                $gambarf=$h['gambar'];
+                            ?>
+                                <li>
+                                    <a href="<?php echo base_url().'paket_tour/detail_paket/'.$idpaketf;?>"><img width="50" height="50" src="<?php echo base_url().'assets/gambars/produk/'.$gambarf;?>" alt="" /><?php echo $namapaketf;?></a>
+                                </li>
+                            <?php } ?> 
+                            </ul>
+                        </div>
+                        <div id="footer-gallery" class="one-fourth column last">
+                            <h4><span class="footer left">Photo Gallery</span></h4>
+                            <ul>
+                                <?php
+                                    foreach ($photo->result_array() as $p):
+                                    $jdl_galeri=$p['jdl_galeri'];
+                                    $gbr_galeri=$p['gbr_galeri'];
+                                ?>
+                                <li>
+                                    <a href="<?php echo base_url().'assets/gambars/gallery/'.$gbr_galeri;?>" class="image-box" rel="beach"><img src="<?php echo base_url().'assets/gambars/gallery/'.$gbr_galeri;?>"  alt="" /></a>
+                                </li>
+                                <?php endforeach ?>
+                            </ul>
+                            <p>
+                                <a href="<?php echo base_url().'detail_photo/galeri';?>">Lihat Semua</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+                <!-- Copyright and Social Icons -->
+                <div id="copyright">
+                    <div class="container section end">
+                        <ul id="social">
+                            <li>
+                                <a href="#"><img src="<?php echo base_url().'theme/images/social/facebook.png'?>" alt="" /></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="<?php echo base_url().'theme/images/social/flickr.png'?>" alt="" /></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="<?php echo base_url().'theme/images/social/twitter.png'?>" alt="" /></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="<?php echo base_url().'theme/images/social/vimeo.png'?>" alt="" /></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="<?php echo base_url().'theme/images/social/rss.png'?>" alt="" /></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="<?php echo base_url().'theme/images/social/google-plus.png'?>" alt="" /></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="<?php echo base_url().'theme/images/social/linkedin.png'?>" alt="" /></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="<?php echo base_url().'theme/images/social/dribbble.png'?>" alt="" /></a>
+                            </li>
+                        </ul>
+                        <span id="text">Copyright &copy; <?php date_default_timezone_set('Asia/Jakarta'); echo date('Y');?> | <a href="http://gentasolusi.com">Genta Informatika</a>.</span>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+            <script type="text/javascript" src="<?php echo base_url().'theme/scripts/jquery-1.7.2.min.js'?>"></script>
+            <script type="text/javascript" src="<?php echo base_url().'theme/scripts/jquery.easing.1.3.js'?>"></script>
+            <script type="text/javascript" src="<?php echo base_url().'theme/scripts/jquery.flexslider-min.js'?>"></script>
+            <script type="text/javascript" src="<?php echo base_url().'theme/scripts/jquery.hoverIntent.minified.js'?>"></script>
+            <script type="text/javascript" src="<?php echo base_url().'theme/scripts/superfish.js'?>"></script>
+            <script type="text/javascript" src="<?php echo base_url().'theme/scripts/jquery.cycle.lite.js'?>"></script>
+            <script type="text/javascript" src="<?php echo base_url().'theme/scripts/jquery.fancybox-1.3.4.pack.js'?>"></script>
+            <script type="text/javascript" src="<?php echo base_url().'theme/scripts/jquery.validationEngine.js'?>"></script>
+            <script type="text/javascript" src="<?php echo base_url().'theme/scripts/jquery.validationEngine-en.js'?>"></script>
+            <script type="text/javascript" src="<?php echo base_url().'theme/scripts/jquery-ui-1.8.22.custom.min.js'?>"></script>
+            <script type="text/javascript" src="<?php echo base_url().'theme/scripts/ui.spinner.min.js'?>"></script>
+            <script type="text/javascript" src="<?php echo base_url().'theme/scripts/lamoon.js'?>"></script>            
+
+    </body>
+</html>
